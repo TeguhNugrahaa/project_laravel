70c204e1fb6fac8cb6b127992fad5d6b1bfa586d
TeguhNugrahaa <38280927+TeguhNugrahaa@users.noreply.github.com>
2023-01-12 18:10:53 +0700

    Update Student.php

--- example-app/app/Models/Student.php
+++ example-app/app/Models/Student.php
@@ -5,11 +5,6 @@ namespace App\Models;
 //use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Student extends Model
+class Students extends Model
 {
-    // kalau nama tabelnya tidak default kalau tidak punya tabel student
-    
-    //dan untuk protected table tidak perlu digunakan
-
-    //protected $table = 'mahasiswa';
 }
