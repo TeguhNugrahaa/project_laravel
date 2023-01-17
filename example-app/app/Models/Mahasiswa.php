73ce52d48d572dee61d313f35e891697078c6138
TeguhNugrahaa <38280927+TeguhNugrahaa@users.noreply.github.com>
2023-01-17 12:21:48 +0700

    Update Mahasiswa.php

--- example-app/app/Models/Mahasiswa.php
+++ example-app/app/Models/Mahasiswa.php
@@ -5,6 +5,7 @@ namespace App\Models;
 //use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Mahasiswa extends Model
 {
     // kalau nama tabelnya tidak default kalau tidak punya tabel student pake protected tabel
@@ -12,4 +13,7 @@ class Mahasiswa extends Model
     //dan untuk protected table untuk mahasiswa
 
     protected $table = 'mahasiswa';
+
+    //mess assignment dengan protected dengan nama, nim, email,jurusan
+    protected $fillable = ['nama', 'nim', 'email', 'jurusan'];
 }
